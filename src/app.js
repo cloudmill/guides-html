76b22bec509b3f82711f12f1bdff841abde70fd4
Nikita <nikita.ableev@mail.ru>
2021-09-21 14:19:18 +0300

    Add fancybox

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import 'Styles/_app.scss'
 
 import Swiper from 'swiper/bundle';
+import '@fancyapps/fancybox';
 
 
 // swiper 
@@ -217,4 +218,27 @@ import Swiper from 'swiper/bundle';
       })
     }
   })
+}
+
+// fancybox
+{
+  $(() => {
+    $.fancybox.defaults.closeExisting = true;
+    $.fancybox.defaults.touch = false;
+    $.fancybox.defaults.baseTpl = (
+      '<div class="fancybox-container" role="dialog" tabindex="-1">' +
+      '<div class="fancybox-bg"></div>' +
+      '<div class="fancybox-stage"></div>' +
+      '</div>'
+    )
+
+    $('[data-fancy-button]').on('click', function (event) {
+      event.preventDefault();
+
+      const id = $(this).data('fancy-button');
+      const modal = $(`[data-fancy-modal="${id}"]`);
+
+      $.fancybox.open(modal);
+    });
+  });
 }
