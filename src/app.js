038508061246382108a187e2d66b156251dbca8e
Nikita <nikita.ableev@mail.ru>
2021-09-21 11:01:12 +0300

    Update accordion

--- src/app.js
+++ src/app.js
@@ -200,3 +200,21 @@ import Swiper from 'swiper/bundle';
     }
   });
 }
+
+// accordion
+{
+  $(() => {
+
+    if ($('[data-accordion]').length) {
+      window.addEventListener('click', (e) => {
+        const accordion = $('[data-accordion]')
+        const target = $(e.target)
+
+        if (target.closest('[data-accordion-button]').length) {
+          target.closest(accordion).toggleClass('active');
+          target.closest(accordion).find('[data-accordion-dropdown]').slideToggle()
+        }
+      })
+    }
+  })
+}
