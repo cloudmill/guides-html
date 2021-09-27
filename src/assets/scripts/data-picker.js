14bce2fff44828aede50d5b36bd6eff1f374aed9
Nikita <nikita.ableev@mail.ru>
2021-09-27 17:07:03 +0300

    Update data picker
    Update counter

--- src/assets/scripts/data-picker.js
+++ src/assets/scripts/data-picker.js
@@ -7,14 +7,12 @@ import flatpickr from "flatpickr";
     if (picker.length) {
       picker.each(function() {
         const ths = $(this)
-        const pickerWrapper = ths.closest('.data-picker-wrapper')
         const pickerInput = ths.find('.data-picker__input')
 
         pickerInput.flatpickr({
           dateFormat: "d.m.Y",
           monthSelectorType: 'static',
-          appendTo: pickerWrapper[0],
-          position: "below center"
+          position: "below left",
         })
       })
     }
