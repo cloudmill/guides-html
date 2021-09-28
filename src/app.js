fbd5a73c197c9d5013f3cb4c0ae25aebd22f8db1
Nikita <nikita.ableev@mail.ru>
2021-09-28 13:27:37 +0300

    Update main page animation

--- src/app.js
+++ src/app.js
@@ -57,24 +57,24 @@ import 'scripts/form-response.js';
 }
 
 // hero animation
-// {
-//   $(() => {
-//     const tab = $('[data-hero-tab]')
+{
+  $(() => {
+    const tab = $('[data-hero-tab]')
     
-//     tab.on('mouseenter', function() {
-//       const id = $(this).data('hero-tab')
-//       const frame = $(`[data-hero-frame=${id}]`)
+    tab.on('mouseenter', function() {
+      const id = $(this).data('hero-tab')
+      const frame = $(`[data-hero-frame=${id}]`)
 
-//       frame.removeClass('hero__frame--hidden')
-//       frame.addClass('hero__frame--front')
-//     })
+      frame.removeClass('hero__frame--hidden')
+      frame.addClass('hero__frame--front')
+    })
 
-//     tab.on('mouseleave', function() {
-//       const id = $(this).data('hero-tab')
-//       const frame = $(`[data-hero-frame=${id}]`)
+    tab.on('mouseleave', function() {
+      const id = $(this).data('hero-tab')
+      const frame = $(`[data-hero-frame=${id}]`)
 
-//       frame.addClass('hero__frame--hidden')
-//       frame.removeClass('hero__frame--front')
-//     })
-//   })
-// }
+      frame.removeClass('hero__frame--front')
+      frame.addClass('hero__frame--hidden')
+    })
+  })
+}
