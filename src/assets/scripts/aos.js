732ddb7d0838877abe4f38e117e93a9f2ca427f6
Nikita <nikita.ableev@mail.ru>
2021-10-05 16:37:04 +0300

    Update AOS

--- src/assets/scripts/aos.js
+++ src/assets/scripts/aos.js
@@ -2,8 +2,20 @@ import AOS from 'aos';
   
 $(() => {
   AOS.init({
-    once: true,
+    once: true, // whether animation should happen only once - while scrolling down
     offset: 0,
     duration: 1000,
   });
-})
+
+  window.addEventListener('scroll', aosRefresh);
+
+  function aosRefresh() {
+    const timeout = setTimeout( () => {
+      clearTimeout(timeout)
+      AOS.refresh();
+      window.addEventListener('scroll', aosRefresh);
+    },1000);
+
+    window.removeEventListener('scroll', aosRefresh);
+  }
+});
