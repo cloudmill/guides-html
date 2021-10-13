fb5b72e21cf43e6035ba8d3e9d2d284a4fb250a9
Nikita <nikita.ableev@mail.ru>
2021-10-13 11:32:06 +0300

    Update admin pages

--- src/assets/scripts/admin-tabs.js
+++ src/assets/scripts/admin-tabs.js
@@ -1,24 +1,11 @@
-// import {Tabs} from './tabs'
-
-// export class AdminTabs {
-//   constructor(name) {
-//     this.root = document.querySelectorAll(name)
-//     this.tabs = document.querySelectorAll('.admin__tab')
-
-//   }
-
-//   setActive(item) {
-
-//   }
-// }
 
 $(() => {
-  const tabs = $('[data-tabs]')
-  const tab = tabs.find('[data-tabs-item]')
+  const tabs = $('[data-admin-tabs]')
+  const tab = tabs.find('[data-admin-item]')
   const tabsContainer = $('.admin__tab')
 
   tab.on('click', function() {
-    const index = $(this).closest('[data-admin-tab]').index()
+    const index = $(this).closest('.product-tabs__item').index()
     
     if (!$(this).hasClass('active')) {
       tab.removeClass('active')
