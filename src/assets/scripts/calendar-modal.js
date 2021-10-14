355b134ea3b72374b2560d5a420ff3c2491b6458
Nikita <nikita.ableev@mail.ru>
2021-10-14 12:17:01 +0300

    Update calendar

--- src/assets/scripts/calendar-modal.js
+++ src/assets/scripts/calendar-modal.js
@@ -1,116 +1,22 @@
 import '@fancyapps/fancybox';
 
-// $(() => {
-
-//   $.fancybox.defaults.closeExisting = true;
-//   $.fancybox.defaults.touch = false;
-//   $.fancybox.defaults.baseTpl = (
-//     '<div class="fancybox-container" role="dialog" tabindex="-1">' +
-//     '<div class="fancybox-bg"></div>' +
-//     '<div class="fancybox-stage"></div>' +
-//     '</div>'
-//   )
-  
-//   const modal = document.querySelector('[data-admin-modal]');
-//   const inputPayment = modal.querySelector('[data-payment-input]') 
-//   const inputStart = modal.querySelector('[data-modal-start]') 
-//   const inputEnd = modal.querySelector('[data-modal-end]') 
-//   const calendarItems = document.querySelectorAll('[data-admin-checkbox-item]') //?
-
-//   const state = {
-//     button: null,
-//     buttonType: null,
-//   }
-
-//   window.addEventListener('click', (event) => {
-//     const modalButton = event.target.closest('[data-admin-button]')
-//     const changeButton = event.target.closest('[data-admin-change-button]')
-
-//     if (modalButton) {
-//       $.fancybox.open(modal);
-//       const buttonId = modalButton.getAttribute('data-admin-button')
-//       state.buttonType = buttonId
-//       state.button = modalButton.closest('[data-admin-button]')
-//     }
-
-//     if (changeButton) {
-//       const select = document.querySelector('[data-admin-select]')
-//       const selectText = select.querySelector('.select2-selection__rendered')
-//       const dataStart = document.querySelector('[data-calendar-start]') // ?
-//       const dataEnd = document.querySelector('[data-calendar-end]') // ?
-
-//       switch (state.buttonType) {
-//         case 'single': {
-//           const day = state.button.querySelector('[data-calendar-day]')
-//           const status = state.button.querySelector('[data-calendar-status]')
-//           const start = state.button.querySelector('[data-calendar-start]')
-//           const end = state.button.querySelector('[data-calendar-end]')
-
-//           status.textContent = selectText.textContent
-//           day.textContent = inputPayment.value + ' дня'
-//           start.textContent = inputStart.value
-//           end.textContent = inputEnd.value
-//           break;
-//         }
-
-//         case 'group': {
-//           let arr = []
-
-//           calendarItems.forEach((item, index) => {
-//             const checkbox = item.querySelector('.checkbox__input')
-//             checkbox.checked ? arr.push(index + 1) : ''
-//           })
-
-//           const selector = arr.map(item => `[data-admin-button="single"]:nth-child(${item})`).join(', ')
-//           const items = document.querySelectorAll(selector)
-
-//           items.forEach(item => {
-//             const status = item.querySelector('[data-calendar-status]')
-//             const day = item.querySelector('[data-calendar-day]')
-//             const start = item.querySelector('[data-calendar-start]')
-//             const end = item.querySelector('[data-calendar-end]')
-            
-//             if (dataStart.value) {
-//               const curDate = new Date(item.getAttribute('data-calendar-data'))
-
-//               if (curDate > new Date(dataStart.value) && curDate < new Date(dataEnd.value)) {
-//                 status.textContent = selectText.textContent
-//                 day.textContent = inputPayment.value + ' дня'
-//               }
-//             }
-
-//             status.textContent = selectText.textContent
-//             day.textContent = inputPayment.value + ' дня'
-//             start.textContent = inputStart.value
-//             end.textContent = inputEnd.value
-//           })
-//           break;
-//         }
-//       }
-
-//       inputPayment.value = ''
-//       $.fancybox.close(modal);
-//     }
-//   })
-// })
-
 class CalendarChanger {
-  constructor() {
+  constructor(selector) {
+    
     $.fancybox.defaults.closeExisting = true;
     $.fancybox.defaults.touch = false;
-    $.fancybox.defaults.baseTpl = (
-      '<div class="fancybox-container" role="dialog" tabindex="-1">' +
-      '<div class="fancybox-bg"></div>' +
-      '<div class="fancybox-stage"></div>' +
-      '</div>'
-    )
-    this.modal = document.querySelector('[data-admin-modal]')
-    this.inputPayment = this.modal.querySelector('[data-payment-input]') 
-    this.inputStart = this.modal.querySelector('[data-modal-start]') 
-    this.inputEnd = this.modal.querySelector('[data-modal-end]') 
-    this.calendarItems = document.querySelectorAll('[data-admin-checkbox-item]') //?
+    
+    this.root = document.querySelector(selector)
+    
+    if (this.root) {
+      this.modal = document.querySelector('[data-admin-modal]')
+      this.inputPayment = this.modal.querySelector('[data-payment-input]') 
+      this.calendarItems = document.querySelectorAll('[data-admin-checkbox-item]') 
+      this.inputStart = this.modal.querySelector('[data-modal-start]') 
+      this.inputEnd = this.modal.querySelector('[data-modal-end]') 
 
-    this.handleModal()
+      this.handleModal()
+    }
   }
 
   handleModal() {
@@ -127,8 +33,8 @@ class CalendarChanger {
       if (changeButton) {
         this.select = document.querySelector('[data-admin-select]')
         this.selectText = this.select.querySelector('.select2-selection__rendered')
-        this.dataStart = document.querySelector('[data-calendar-start]') // ?
-        this.dataEnd = document.querySelector('[data-calendar-end]') // ?
+        this.changeStart = document.querySelector('[data-change-start]') // ?
+        this.changeEnd = document.querySelector('[data-change-end]') // ?
 
         switch (this.buttonId) {
           case 'single': {
@@ -143,29 +49,29 @@ class CalendarChanger {
               const checkbox = item.querySelector('.checkbox__input')
               checkbox.checked ? arr.push(index + 1) : ''
             })
+            
+            if (arr.length) {
+              const selector = arr.map(item => `[data-admin-button="single"]:nth-child(${item})`).join(', ')
+              const items = document.querySelectorAll(selector)
 
-            const selector = arr.map(item => `[data-admin-button="single"]:nth-child(${item})`).join(', ')
-            const items = document.querySelectorAll(selector)
+              items.forEach(item => {
+                this.getItems(item)
 
-            items.forEach(item => {
-              this.getItems(item)
-              
-              if (this.dataStart.value) {
-                const curDate = new Date(item.getAttribute('data-calendar-data'))
+                if (this.changeStart.value) {
+                  const curDate = new Date(item.getAttribute('data-calendar-data'))
 
-                if (curDate > new Date(this.dataStart.value) && curDate < new Date(this.dataEnd.value)) {
+                  if (curDate > new Date(this.changeStart.value) && curDate < new Date(this.changeEnd.value)) {
+                    this.setItems()
+                  }
+                } else {
                   this.setItems()
                 }
-              }
-
-              this.setItems()
-            })
+              })
+            }
             break;
           }
         }
-
-        this.inputPayment.value = ''
-        $.fancybox.close(this.modal);
+        this.closeModal()
       }
     })
   }
@@ -173,21 +79,29 @@ class CalendarChanger {
   getItems(selector) {
     this.day = selector.querySelector('[data-calendar-day]')
     this.status = selector.querySelector('[data-calendar-status]')
-    this.start = selector.querySelector('[data-calendar-start]')
-    this.end = selector.querySelector('[data-calendar-end]')
+    this.startTime = selector.querySelector('[data-calendar-start]')
+    this.endTime = selector.querySelector('[data-calendar-end]')
   }
 
   setItems() {
     this.day.textContent = this.inputPayment.value + ' дня'
     this.status.textContent = this.selectText.textContent
-    if (this.start) {
-      this.start.textContent = this.inputStart.value
-      this.end.textContent = this.inputEnd.value
+    if (this.startTime) {
+      this.startTime.textContent = this.inputStart.value
+      this.endTime.textContent = this.inputEnd.value
+    }
+  }
+
+  closeModal() {
+    this.inputPayment.value = ''
+    $.fancybox.close(this.modal);
+    if (this.inputStart) {
+      this.inputStart.value = ''
+      this.inputEnd.value = ''
     }
   }
 }
 
 $(() => {
-  const calendar = new CalendarChanger()
-
+  const calendar = new CalendarChanger('.calendar-card')
 })
