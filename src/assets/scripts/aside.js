5808316a996dae376b699e552357a9f39e0f1c48
Nikita <nikita.ableev@mail.ru>
2022-02-01 15:56:13 +0300

    #Build project

--- src/assets/scripts/aside.js
+++ src/assets/scripts/aside.js
@@ -6,20 +6,20 @@ $(() => {
   if (aside.length) {
     const checkbox = aside.find('.aside__input')
 
-    checkbox.on('change', function() {
-      const item = $(this).closest('.aside__item')
-      const itemLabel = item.find('.aside__select')
-      const itemCheckbox = item.find('.aside__input')
+    // checkbox.on('change', function() {
+    //   const item = $(this).closest('.aside__item')
+    //   const itemLabel = item.find('.aside__select')
+    //   const itemCheckbox = item.find('.aside__input')
 
-      itemCheckbox.each(function() {
-        if (this.checked === true) {
-          itemLabel.removeClass('disabled')
-          return false
-        } else {
-          itemLabel.addClass('disabled')
-        }
-      })
-    })
+    //   itemCheckbox.each(function() {
+    //     if (this.checked === true) {
+    //       itemLabel.removeClass('disabled')
+    //       return false
+    //     } else {
+    //       itemLabel.addClass('disabled')
+    //     }
+    //   })
+    // })
     
     // Количество выбранных фильтров
     const select = $('[data-aside-select]')
