44bacdf3536a0a0a3a885247631b05fb1861fff5
Nikita <nikita.ableev@mail.ru>
2022-03-09 12:57:47 +0300

    Update

--- src/assets/scripts/header.js
+++ src/assets/scripts/header.js
@@ -3,47 +3,50 @@
 {
   $(() => {
     const header = $('.header')
+    const staticHeader = $('[data-header-static]')
 
-    const fps = 120
-
-    let scrollTop = $(window).scrollTop()
-
-    $(window).one('scroll', scroll)
-
-    function scroll() {
-      update()
-
-      setTimeout(() => {
+    if (!staticHeader.length) {
+      const fps = 120
+  
+      let scrollTop = $(window).scrollTop()
+  
+      $(window).one('scroll', scroll)
+  
+      function scroll() {
         update()
-
-        $(window).one('scroll', scroll)
-      }, 1000 / fps)
-    }
-
-    function update() {
-      const newScrollTop = $(window).scrollTop()
-
-      if (Math.abs(scrollTop - newScrollTop) >= 1) {
-        if (newScrollTop > scrollTop) {
-          header.addClass('header--up')
-        } else {
+  
+        setTimeout(() => {
+          update()
+  
+          $(window).one('scroll', scroll)
+        }, 1000 / fps)
+      }
+  
+      function update() {
+        const newScrollTop = $(window).scrollTop()
+  
+        if (Math.abs(scrollTop - newScrollTop) >= 1) {
+          if (newScrollTop > scrollTop) {
+            header.addClass('header--up')
+          } else {
+            header.removeClass('header--up')
+          }
+        }
+  
+        if (scrollTop < 1) {
           header.removeClass('header--up')
         }
-      }
-
-      if (scrollTop < 1) {
-        header.removeClass('header--up')
-      }
-
-      if ($('[data-header-transparent]').length) {
-        if (scrollTop < 1) {
-          header.addClass('header--transparent')
-        } else {
-          header.removeClass('header--transparent')
+  
+        if ($('[data-header-transparent]').length) {
+          if (scrollTop < 1) {
+            header.addClass('header--transparent')
+          } else {
+            header.removeClass('header--transparent')
+          }
         }
+  
+        scrollTop = newScrollTop
       }
-
-      scrollTop = newScrollTop
     }
   });
 }
