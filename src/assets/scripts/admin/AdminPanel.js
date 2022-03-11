59e0870a261ad7e933569522de6ab956ffd8462e
Nikita <nikita.ableev@mail.ru>
2022-03-11 19:33:04 +0300

    Update admin panel

--- src/assets/scripts/admin/AdminPanel.js
+++ src/assets/scripts/admin/AdminPanel.js
@@ -1,3 +1,6 @@
+import {initSelect} from '../select'
+import 'select2';
+
 class AdminPanel {
   constructor() {
     
@@ -9,17 +12,37 @@ class AdminPanel {
       const target = event.target
 
       if (target.hasAttribute('data-add-button')) {
-        this.addElement(target)
+        const attrValue = target.getAttribute('data-add-button')
+
+        if (attrValue === "default") {
+          this.addElement(target)
+        }
+
+        if (attrValue === "select") {
+          this.addSelect(target)
+        }
       }
     })
   }
 
   addElement(button) {
     const parent = button.closest('[data-add-parent]')
-    const container = parent.querySelector('[data-add-container]')
     const clone = parent.querySelector('[data-add-item]').cloneNode(true)
     
-    container.append(clone);
+    parent.querySelector('[data-add-container]').append(clone);
+  }
+
+  addSelect(button) {
+    const parent = button.closest('[data-add-parent]')
+    const item = parent.querySelector('[data-add-item]')
+    const select = $(item.querySelector('.select__select'))
+
+    select.select2('destroy')
+    const clone = item.cloneNode(true)
+    initSelect(select)
+
+    parent.querySelector('[data-add-container]').append(clone);
+    initSelect($(clone.querySelector('.select__select')))
   }
 }
 
