b662baf2c422db22197bf701a48eec550f08b401
Nikita <nikita.ableev@mail.ru>
2022-03-17 17:28:47 +0300

    Update thumb-slider

--- src/assets/scripts/swipers.js
+++ src/assets/scripts/swipers.js
@@ -82,59 +82,65 @@ const BREAKPOINT_MOBILE = 768
 
 			const slider = $('.thumb-slider');
 
-			if (slider.length !== 0) {
-				const thumbsSwiperEl = slider.find('.thumb-slider__thumbs');
-        
-				const thumbsSwiper = new Swiper(thumbsSwiperEl[0], {
-					speed: SPEED,
-					slidesPerView: 'auto',
-					spaceBetween: 10,
-				});
+			if (slider.length) {
+        const images = slider.find('.thumb-slider__slide-img')
 
-				const thumbsSlide = slider.find('.thumb-slider__thumb');
-
-				const sliderSwiperEl = slider.find('.swiper-container');
-				const sliderSwiper = new Swiper(sliderSwiperEl[0], {
-					speed: SPEED,
-				});
-
-        const buttonPrev = slider.find('.slider-buttons__button--left')
-        const buttonNext = slider.find('.slider-buttons__button--right')
-
-				// control
-				let lastAction = 'nothing';
-				sliderSwiper.on('sliderFirstMove', () => {
-					lastAction = 'slider swipe';
-				})
-				thumbsSwiper.on('click', event => {
-					lastAction = 'thumbs click';
-
-					sliderSwiper.slideTo(event.clickedIndex);
-
-					thumbsSlide.removeClass('thumb-slider__thumb--active');
-					thumbsSlide.eq(event.clickedIndex).addClass('thumb-slider__thumb--active');
-				});
-
-        buttonPrev.on('click', function() {
-          lastAction = 'slider swipe'
-          sliderSwiper.slidePrev()
-        })
-
-        buttonNext.on('click', function() {
-          lastAction = 'slider swipe'
-          sliderSwiper.slideNext()
-        })
-
-				sliderSwiper.on('slideChange', event => {
-					if (lastAction === 'slider swipe') {
-						thumbsSwiper.slideTo(event.realIndex);
-
-						thumbsSlide.removeClass('thumb-slider__thumb--active');
-						thumbsSlide.eq(event.realIndex).addClass('thumb-slider__thumb--active');
-					}
-
-					lastAction = 'nothing';
-				});
+        if (images.length > 1) {
+          const thumbsSwiperEl = slider.find('.thumb-slider__thumbs');
+          
+          const thumbsSwiper = new Swiper(thumbsSwiperEl[0], {
+            speed: SPEED,
+            slidesPerView: 'auto',
+            spaceBetween: 10,
+          });
+  
+          const thumbsSlide = slider.find('.thumb-slider__thumb');
+  
+          const sliderSwiperEl = slider.find('.swiper-container');
+          const sliderSwiper = new Swiper(sliderSwiperEl[0], {
+            speed: SPEED,
+          });
+  
+          const buttonPrev = slider.find('.slider-buttons__button--left')
+          const buttonNext = slider.find('.slider-buttons__button--right')
+  
+          // control
+          let lastAction = 'nothing';
+          sliderSwiper.on('sliderFirstMove', () => {
+            lastAction = 'slider swipe';
+          })
+          thumbsSwiper.on('click', event => {
+            lastAction = 'thumbs click';
+  
+            sliderSwiper.slideTo(event.clickedIndex);
+  
+            thumbsSlide.removeClass('thumb-slider__thumb--active');
+            thumbsSlide.eq(event.clickedIndex).addClass('thumb-slider__thumb--active');
+          });
+  
+          buttonPrev.on('click', function() {
+            lastAction = 'slider swipe'
+            sliderSwiper.slidePrev()
+          })
+  
+          buttonNext.on('click', function() {
+            lastAction = 'slider swipe'
+            sliderSwiper.slideNext()
+          })
+  
+          sliderSwiper.on('slideChange', event => {
+            if (lastAction === 'slider swipe') {
+              thumbsSwiper.slideTo(event.realIndex);
+  
+              thumbsSlide.removeClass('thumb-slider__thumb--active');
+              thumbsSlide.eq(event.realIndex).addClass('thumb-slider__thumb--active');
+            }
+  
+            lastAction = 'nothing';
+          });
+        } else {
+          slider.addClass('static')
+        }
 			}
 		}
 	});
