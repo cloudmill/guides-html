9fe4600d222b157c2fe9f58880a6f0eb9b7f18dd
cloudmill <info@cloudmill.ru>
2022-04-05 15:46:18 +0300

    update

--- src/assets/scripts/backend.js
+++ src/assets/scripts/backend.js
@@ -10,9 +10,16 @@ $(function() {
 });
 
 window.addEventListener('dayAdded', function(e) {
-  const container = $(e.detail.container);
+  const container = $(e.detail.container),
+    parent = container.parents('.admin__tab'),
+    data = parent.data('input');
 
-  container.append(`<input type="hidden" data-type="get-field" data-field="PROPERTY_VALUES[HIGHLOAD][7][DATA][UF_DAY]" value="${e.detail.count}">`);
+  container.append(`<input type="hidden" data-type="get-field" data-field="${data.highload}[${e.detail.count}][UF_DAY]" value="${e.detail.count}">`);
+  container.find('[data-type=get-field]').each(function() {
+    const field = $(this).data('field');
+
+    $(this).attr('data-field', `${data.highload}[${e.detail.count}][${field}][]`);
+  });
 });
 
 function removeItem() {
